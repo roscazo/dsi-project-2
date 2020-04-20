ea6a8ab842e0246355c96fc8e73cfed247fd96f8
roscazo <roskoelmolon@gmail.com>
2020-04-20 18:22:17 +0200

    8 files modified

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 const MaterialComponents = [
   MatButtonModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatIconModule
 ];
 
 @NgModule({
